9d915f8f78cd38f0197fb95f2abb11e8412903fb
Rakesh Baruah <rbaruah@gmail.com>
2024-11-06 11:01:33 -0600

    implement server side search form

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import Form from "next/form";
+import SearchFormReset from "./SearchFormReset";
 
-const SearchForm = () => {
-  return <div>SearchForm</div>;
+const SearchForm = ({ query }: { query?: string }) => {
+  return (
+    <Form action="/" scroll={false} className="search-form">
+      <input
+        name="query"
+        defaultValue={query}
+        placeholder="Search for startups"
+        className="search-input"
+      />
+      <div className="flex gap-2">
+        {query && <SearchFormReset />}
+        <button type="submit" className="search-btn text-white">
+          S
+        </button>
+      </div>
+    </Form>
+  );
 };
 
 export default SearchForm;
